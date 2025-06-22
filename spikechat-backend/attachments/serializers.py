b9dfcc4a39a78288e6bef65bc40a89f59e486d5d
TakashiNishii <ygortn14@gmail.com>
2025-06-22 11:22:14 -0400

    Adiciona serializers para FileAttachment e AudioAttachment com formatação de tamanho e URL

---
+++ spikechat-backend/attachments/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from attachments.models import FileAttachment, AudioAttachment
+from attachments.utils.formatter import Formatter
+
+from django.conf import settings
+
+class FileAttachmentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = FileAttachment
+    fields = '__all__'
+
+  def to_representation(self, instance):
+    data = super().to_representation(instance)
+    data['size'] = Formatter.format_bytes(instance.size)
+    data['src'] = f"{settings.MEDIA_URL}{instance.src}"
+
+    return data
+  
+class AudioAttachmentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = AudioAttachment
+    fields = '__all__'
+
+  def to_representation(self, instance):
+    data = super().to_representation(instance)
+    data['src'] = f"{settings.MEDIA_URL}{instance.src}"
+
+    return data
