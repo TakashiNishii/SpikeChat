7da61aca22912b2c8caa9c537df8da021f81ff28
TakashiNishii <ygortn14@gmail.com>
2025-08-28 20:35:03 -0400

    feat: adiciona componente MainLayout e implementa o sistema de carregamento com react-spinners

---
+++ spikechat-frontend/src/components/Layouts/MainLayout.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { ReactNode, useEffect, useState } from "react"
+import { User } from "../../types/User"
+import { useAuthStore } from "../../stores/authStore"
+import { useChatStore } from "../../stores/chatStore"
+import { usePathname } from "next/navigation"
+import { Header } from "./Header"
+import { BarLoader } from "react-spinners"
+import { LeftSide } from "./LeftSide"
+
+import {
+  Sheet,
+  SheetContent,
+} from "@/components/ui/sheet"
+
+type Props = {
+  user: User | null,
+  children: ReactNode
+}
+
+export const MainLayout = ({ user, children }: Props) => {
+  const auth = useAuthStore()
+  const { showChatsList, setShowChatsList } = useChatStore()
+
+  const [loading, setLoading] = useState(true)
+
+  const pathname = usePathname()
+
+  useEffect(() => {
+    if (user) auth.setUser(user)
+
+    setLoading(false)
+  }, [])
+
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-slate-200 dark:bg-slate-950">
+      <Header />
+
+      {loading &&
+        <div className="flex items-center justify-center h-full">
+          <BarLoader color="#493CDD" />
+        </div>
+      }
+
+      {!loading && auth.user && !pathname.includes('auth') ?
+        <div className="flex h-full">
+          <div className="hidden lg:block">
+            <LeftSide />
+          </div>
+
+          <div className="flex-1">
+            {children}
+          </div>
+        </div>
+        :
+        <div className="flex-1">
+          {children}
+        </div>
+      }
+
+      <Sheet open={showChatsList} onOpenChange={setShowChatsList}>
+        <SheetContent className="p-0 bg-slate-100 dark:bg-slate-900">
+          <LeftSide variant="mobile" />
+        </SheetContent>
+      </Sheet>
+    </div>
+  )
+}
